98e3663d8c1a77e4d2d1f3e5aa62d4db69a6debb
Akira Saito <akrstjp@users.sourceforge.jp>
2014-08-29 00:18:26 +0900

    all relation type support

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 RedmineApp::Application.routes.draw do
+
+  post 'quick_edit_relations', :controller => 'quick_edit_relations', :action => 'create'
   resources :issues do
     shallow do
-      #resources :relations, :controller => 'issue_relations', :only => [:index, :show, :create, :destroy]
       resources :quick_edit_relations, :controller => 'quick_edit_relations', :only => [:create, :destroy]
     end
   end
-
-#  resources :project do
-#  end
 end
 
