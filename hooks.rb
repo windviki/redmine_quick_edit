87a08dc94280023e2c8ee46c86d0c824e9fadccb
Akira Saito <akrstjp@users.sourceforge.jp>
2015-01-18 19:50:02 +0900

    fix the collision of the template name.

--- hooks.rb
+++ hooks.rb
@@ -1,6 +1,6 @@
 #coding: utf-8
 
 class QuickEditHooks < Redmine::Hook::ViewListener
-	render_on :view_layouts_base_html_head, :partial=> 'hooks/head'
-	render_on :view_issues_context_menu_end, :partial=> 'hooks/context'
+	render_on :view_layouts_base_html_head, :partial=> 'hooks/quick_edit_base_head'
+	render_on :view_issues_context_menu_end, :partial=> 'hooks/quick_edit_context'
 end
