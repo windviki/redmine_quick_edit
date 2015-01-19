bc71d06807a97aa3388039cdd71d9a7204136922
Akira Saito <akrstjp@users.sourceforge.jp>
2015-01-19 23:38:09 +0900

    html/javascript of the dialogs move to template of the layout_body_bottom from context menu response.

--- hooks.rb
+++ hooks.rb
@@ -2,5 +2,6 @@
 
 class QuickEditHooks < Redmine::Hook::ViewListener
 	render_on :view_layouts_base_html_head, :partial=> 'hooks/quick_edit_base_head'
+	render_on :view_layouts_base_body_bottom, :partial=> 'hooks/quick_edit_base_bottom'
 	render_on :view_issues_context_menu_end, :partial=> 'hooks/quick_edit_context'
 end
