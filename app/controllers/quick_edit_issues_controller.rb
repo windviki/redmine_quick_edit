631bb9dcbdf82535d5e85e8a7aabde6c5aa02c81
akrstjp <akrst@users.osdn.me>
2016-04-14 20:45:12 +0900

    bugfix refs #36243

--- app/controllers/quick_edit_issues_controller.rb
+++ app/controllers/quick_edit_issues_controller.rb
@@ -88,7 +88,8 @@ private
     unless @attribute_name == :notes
       unless @issue.safe_attribute_names.include?(@attribute_name)
         logger.warn "### quick edit ### no safe attribute. target_specifier=" + @target_specifier
-        render_404
+        @error_message = l(:text_can_not_edit)
+        render
       end
     end
 
